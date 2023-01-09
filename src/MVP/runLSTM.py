fccd0cec0ab5c98e352b28392d512e31c3b867fb
Sebastien <sebastien.garneau@gmail.com>
2023-01-08 19:36:31 -0500

    BTC Trained. Working on training a generic model

--- src/MVP/runLSTM.py
+++ src/MVP/runLSTM.py
@@ -4,17 +4,20 @@ def runLSTM_1D_1S (df, target = 'Close', window = 10, filename = 'undefined', tr
 
     import pandas as pd
     import numpy as np
+    import keras
     from tensorflow.keras.models import Sequential
     from tensorflow.keras.layers import Dense, Dropout, LSTM
     from tensorflow.keras.callbacks import EarlyStopping
-    from sklearn.preprocessing import MinMaxScaler, StandardScaler
+    from tensorflow.keras.models import load_model
+    from sklearn.preprocessing import MinMaxScaler, StandardScaler, Normalizer
     from sklearn.metrics import mean_squared_error
     from sklearn.model_selection import TimeSeriesSplit
 
-    #drop dividend, index, symbol
-    df = df.drop(['Dividends', 'Stock Splits', 'symbol'], axis=1)
+    # print what symbol you are training and deleting the column after. We only kept it for the print statement
+    print('Symbol: ', df['symbol'][0])
+    df = df.drop(['symbol'], axis=1) #, 'Open', 'High', 'Low', 'Volume'
 
-    # Set X , ensuring 'Target' is the last column (set X without then concat back in)
+    # Set X , ensuring 'Target' is the first column (set X without then concat back in)
     X = df.drop(target, axis=1)
     y = df[target]
     X = pd.concat([X, y], axis=1)
@@ -22,6 +25,7 @@ def runLSTM_1D_1S (df, target = 'Close', window = 10, filename = 'undefined', tr
     # Scale the data
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
+
     X = pd.DataFrame(columns=X.columns
                     , data=X_scaled
                     , index=X.index)
@@ -51,12 +55,10 @@ def runLSTM_1D_1S (df, target = 'Close', window = 10, filename = 'undefined', tr
     X_Predict.append(X.values[len(X) - window:, ])
     X_Predict = np.array(X_Predict)
 
-    # print whether X_Test and X_Predict have NaN values
-    print('X_Test NaN: ', np.isnan(X_test).any())
-    print('X_Predict NaN: ', np.isnan(X_Predict).any())
-
-
     #Check to see if we already have a trained model URL=../models/ + filename + '.h5'
+    import os.path
+    from os import path
+
     fileExists = os.path.isfile('../models/' + filename + '.h5')
 
     # If we don't have a trained model, train one
@@ -64,24 +66,21 @@ def runLSTM_1D_1S (df, target = 'Close', window = 10, filename = 'undefined', tr
 
         #Build the model
         lstm = Sequential()
-        lstm.add(LSTM(120, activation='relu', return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
+        lstm.add(LSTM(50, activation='relu', return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
                 
-        lstm.add(LSTM(60, activation='relu', return_sequences=True))
-                
-        lstm.add(LSTM(120, activation='relu', return_sequences=True))
-                
-        lstm.add(LSTM(60, activation='relu', return_sequences=False))
+        # lstm.add(LSTM(50, activation='relu', return_sequences=True))       
+        # lstm.add(LSTM(120, activation='relu', return_sequences=True))
+        
+        lstm.add(LSTM(50, activation='relu', return_sequences=False))
         lstm.add(Dropout(rate=0.2))
-
         lstm.add(Dense(1))
         lstm.compile(optimizer='adam', loss='mse')
-        # lstm.summary()
 
         # Train the model
         lstm.fit(X_train, y_train, epochs=25, batch_size=5, verbose=1, shuffle=False)
 
         # Save trained model (pickle) and add date to file name
-        lstm.save('../models/' + filename + '.h5')
+        # lstm.save('../models/' + filename + '.h5')
     
     else:
         # load model (pickle)
@@ -114,11 +113,6 @@ def runLSTM_1D_1S (df, target = 'Close', window = 10, filename = 'undefined', tr
     y_pred_unscaled = y_pred_unscaled[:,0] 
     y_pred_future_unscaled = y_pred_future_unscaled[:,0]
 
-    #print whether y_test_unscaled and y_pred_unscaled have NaN values
-    print('y_test_unscaled NaN: ', np.isnan(y_test_unscaled).any())
-    print('y_pred_unscaled NaN: ', np.isnan(y_pred_unscaled).any())
-
     rmse = np.sqrt(mean_squared_error(y_test_unscaled, y_pred_unscaled))
-
-    return y_pred_unscaled, y_pred_future_unscaled, rmse
     
+    return y_test_unscaled, y_pred_unscaled, y_pred_future_unscaled, rmse
